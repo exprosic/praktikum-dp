64b18ba891b6ce57d9e0b5b6dcae0ac2ff67e737
exprosic <exprosic@gmail.com>
2017-05-14 01:29:38 +0200

    consistent_binary finished

--- Dynamic_Programming.thy
+++ Dynamic_Programming.thy
@@ -32,33 +32,16 @@ definition plus_state (infixl "+\<^sub>s" 65) where "op +\<^sub>s \<equiv> lift_
 definition min\<^sub>s where "min\<^sub>s \<equiv> lift_binary min"
 definition max\<^sub>s where "max\<^sub>s \<equiv> lift_binary max"
 
-definition pcorrect :: "('param \<Rightarrow> 'result) \<Rightarrow> ('param \<rightharpoonup> 'result) \<Rightarrow> bool" where
-  "pcorrect f M \<equiv> \<forall>param\<in>dom M. M param = Some (f param)"
+definition consistentM :: "('param \<Rightarrow> 'result) \<Rightarrow> ('param \<rightharpoonup> 'result) \<Rightarrow> bool" where
+  "consistentM f M \<equiv> \<forall>param\<in>dom M. M param = Some (f param)"
   
 definition consistentDF :: "('param \<Rightarrow> 'result) \<Rightarrow> ('param, 'result) dpfun \<Rightarrow> bool" where
-  "consistentDF f d\<equiv> \<forall>M. pcorrect f M \<longrightarrow> (\<forall>param. fst (d param M) = f param)"
+  "consistentDF f d \<equiv> \<forall>M. consistentM f M \<longrightarrow> (\<forall>param. let (v,M')=d param M in v=f param \<and> consistentM f M')"
   
-lemma "consistentDF bf bfmem"
-  oops
+lemma "consistentDF s0 \<Longrightarrow> consistentDF s1 \<Longrightarrow>"    
+
     
-lemma pcorrect_upd:
-  "pcorrect f M \<Longrightarrow> v = f param \<Longrightarrow> pcorrect f (M(param\<mapsto>v))"
-  by (simp add: pcorrect_def)
-
-lemma pcorrect_not_None:
-  "pcorrect f M \<Longrightarrow> v = f param \<Longrightarrow> param \<in> dom M \<Longrightarrow> M param = Some (f param)"
-  by(auto simp: pcorrect_def dom_def)
-
-lemma map_le_updR: "M \<subseteq>\<^sub>m M' \<Longrightarrow> param \<notin> dom M \<Longrightarrow> M \<subseteq>\<^sub>m M'(param \<mapsto> v)"
-  by (auto simp: dom_def map_le_def)
-
-lemma map_le_id_upR: "pcorrect f M \<Longrightarrow> f param = v \<Longrightarrow> M \<subseteq>\<^sub>m M(param \<mapsto> v)"
-  by (auto simp: pcorrect_def map_le_def)
-
-lemma fib'_correct_aux: "pcorrect bf M \<Longrightarrow> M \<subseteq>\<^sub>m snd (bf' param M)"
-  apply (induction arbitrary: M rule: bf.induct)
-  oops
-    
+term 0 (*    
 (* Fib *)
 
 fun fib :: "nat \<Rightarrow> nat" where
@@ -76,36 +59,36 @@ fun fib' :: "(nat, nat) dpfun" where
 lemma "fst (fib' 0 empty) = fib 0"
   by (auto)
 
-lemma "pcorrect fib M \<Longrightarrow> fst (fib' 0 M) = fib 0"
-  unfolding pcorrect_def
+lemma "consistentM fib M \<Longrightarrow> fst (fib' 0 M) = fib 0"
+  unfolding consistentM_def
   apply (cases "M 0")
    apply (auto simp: dom_def)
   done
 
-lemma "pcorrect fib M \<Longrightarrow> fst (fib' 1 M) = fib 1"
+lemma "consistentM fib M \<Longrightarrow> fst (fib' 1 M) = fib 1"
   apply (cases "M 1")
-   apply (auto simp: dom_def pcorrect_def)
+   apply (auto simp: dom_def consistentM_def)
   done
 
-lemma "pcorrect fib M \<Longrightarrow> pcorrect fib (snd (fib' n M))"
+lemma "consistentM fib M \<Longrightarrow> consistentM fib (snd (fib' n M))"
   apply (induction n arbitrary: M rule: fib.induct)
-    apply (auto simp: dom_def pcorrect_def split: option.splits)[]
-   apply (auto simp: dom_def pcorrect_def split: option.splits)[]
+    apply (auto simp: dom_def consistentM_def split: option.splits)[]
+   apply (auto simp: dom_def consistentM_def split: option.splits)[]
   oops
 
-lemma "pcorrect fib M \<Longrightarrow> fst (fib' n M) = fib n \<and> pcorrect fib (snd (fib' n M))"
+lemma "consistentM fib M \<Longrightarrow> fst (fib' n M) = fib n \<and> consistentM fib (snd (fib' n M))"
 proof (induction n arbitrary: M rule: fib.induct)
   case 1
-  then show ?case by (auto simp: dom_def pcorrect_def split: option.splits)
+  then show ?case by (auto simp: dom_def consistentM_def split: option.splits)
 next
   case 2
-  then show ?case by (auto simp: dom_def pcorrect_def split: option.splits)
+  then show ?case by (auto simp: dom_def consistentM_def split: option.splits)
 next
   case (3 n)
   obtain v0 M0 where l00:"fib' (Suc n) M = (v0, M0)" by fastforce
-  with 3 have l01:"v0 = fib (Suc n) \<and> pcorrect fib M0" by fastforce
+  with 3 have l01:"v0 = fib (Suc n) \<and> consistentM fib M0" by fastforce
   obtain v1 M1 where l10:"fib' n M0 = (v1, M1)" by fastforce
-  with 3 l01 have l11:"v1 = fib n \<and> pcorrect fib M1" by fastforce
+  with 3 l01 have l11:"v1 = fib n \<and> consistentM fib M1" by fastforce
   from 3 l00 l01 l10 l11 have "fst ((fib' (Suc n) +\<^sub>s fib' n) M) = v0+v1"
     unfolding plus_state_def lift_binary_def by auto
       with l00 l01 l10 l11 show ?case sorry

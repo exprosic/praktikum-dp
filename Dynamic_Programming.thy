9ffa990cf1bfc933bf7f056b4056e07137c8b404
exprosic <exprosic@gmail.com>
2017-06-01 19:10:39 +0200

    minor fix

--- Dynamic_Programming.thy
+++ Dynamic_Programming.thy
@@ -55,10 +55,10 @@ fun ed :: "(nat\<times>nat) \<Rightarrow> nat" where
   "ed (Suc i, Suc j) = min (ed (i, j) + 2) (min (ed (Suc i, j) + 1) (ed (i, Suc j) + 1))"
 
 fun ed'  :: "nat\<times>nat \<Rightarrow>\<^sub>s nat" where
-  "ed'$ (0, 0) = \<langle>0\<rangle>" |
-  "ed'$ (0, Suc j) = \<langle>Suc j\<rangle>" |
-  "ed'$ (Suc i, 0) = \<langle>Suc i\<rangle>" |
-  "ed'$ (Suc i, Suc j) = min\<^sub>s (ed' (i, j) +\<^sub>s \<langle>2\<rangle>) (min\<^sub>s (ed' (Suc i, j) +\<^sub>s \<langle>1\<rangle>) (ed' (i, Suc j) +\<^sub>s \<langle>1\<rangle>))"
+  "ed'$ (0, 0) =CHECKMEM= \<langle>0\<rangle>" |
+  "ed'$ (0, Suc j) =CHECKMEM= \<langle>Suc j\<rangle>" |
+  "ed'$ (Suc i, 0) =CHECKMEM= \<langle>Suc i\<rangle>" |
+  "ed'$ (Suc i, Suc j) =CHECKMEM= min\<^sub>s (ed' (i, j) +\<^sub>s \<langle>2\<rangle>) (min\<^sub>s (ed' (Suc i, j) +\<^sub>s \<langle>1\<rangle>) (ed' (i, Suc j) +\<^sub>s \<langle>1\<rangle>))"
 
 lemma "consistentDF ed ed'"
   by (dp_match induct: ed.induct simp: ed.simps ed'.simps)
